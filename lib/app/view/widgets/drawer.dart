d73f4210b7170e06e5c65b53a529d6b63d4a0631
JamesMutahi <mutahijames0@gmail.com>
2025-12-15 15:02:18 +0300

    move theme toggle buttons to settings page

--- lib/app/view/widgets/drawer.dart
+++ lib/app/view/widgets/drawer.dart
@@ -1,4 +1,3 @@
-import 'package:democracy/app/bloc/theme/theme_cubit.dart';
 import 'package:democracy/app/bloc/websocket/websocket_bloc.dart';
 import 'package:democracy/app/utils/dialogs.dart';
 import 'package:democracy/app/view/widgets/bookmarks.dart';
@@ -90,7 +89,6 @@ class AppDrawer extends StatelessWidget {
                 title: Text('Settings'),
                 leading: Icon(Symbols.settings_rounded),
               ),
-              ThemeModeListTile(),
             ],
           ),
           BlocBuilder<AuthBloc, AuthState>(
@@ -114,91 +112,6 @@ class AppDrawer extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class ThemeModeListTile extends StatelessWidget {
-  const ThemeModeListTile({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<ThemeCubit, ThemeState>(
-      builder: (context, state) {
-        List<bool> isSelected = [false, false, false];
-        ThemeMode themeMode = ThemeMode.system;
-        if (state.index != null) {
-          themeMode = ThemeMode.values[state.index!];
-        }
-        switch (themeMode) {
-          case ThemeMode.light:
-            isSelected = [true, false, false];
-            break;
-          case ThemeMode.system:
-            isSelected = [false, true, false];
-            break;
-          case ThemeMode.dark:
-            isSelected = [false, false, true];
-            break;
-        }
-        return ListTile(
-          leading: const Icon(Symbols.settings_brightness_rounded),
-          title: const Text('Theme'),
-          onTap: () {
-            switch (themeMode) {
-              case ThemeMode.light:
-                context.read<ThemeCubit>().change(
-                  index: ThemeMode.system.index,
-                );
-                break;
-              case ThemeMode.system:
-                context.read<ThemeCubit>().change(index: ThemeMode.dark.index);
-                break;
-              case ThemeMode.dark:
-                context.read<ThemeCubit>().change(index: ThemeMode.light.index);
-                break;
-            }
-          },
-          trailing: ThemeModeToggleButtons(isSelected: isSelected),
-        );
-      },
-    );
-  }
-}
-
-@immutable
-class ThemeModeToggleButtons extends StatelessWidget {
-  const ThemeModeToggleButtons({super.key, required this.isSelected});
-
-  final List<bool> isSelected;
-
-  @override
-  Widget build(BuildContext context) {
-    return ToggleButtons(
-      isSelected: isSelected,
-      onPressed: (int newIndex) {
-        if (newIndex == 0) {
-          context.read<ThemeCubit>().change(index: ThemeMode.light.index);
-        } else if (newIndex == 1) {
-          context.read<ThemeCubit>().change(index: ThemeMode.system.index);
-        } else {
-          context.read<ThemeCubit>().change(index: ThemeMode.dark.index);
-        }
-      },
-      children: const <Widget>[
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 9.5),
-          child: Icon(Icons.wb_sunny),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 9.5),
-          child: Icon(Icons.phone_iphone),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 9.5),
-          child: Icon(Icons.bedtime),
-        ),
-      ],
     );
   }
 }
--- lib/app/view/widgets/settings.dart
+++ lib/app/view/widgets/settings.dart
@@ -1,6 +1,9 @@
+import 'package:democracy/app/bloc/theme/theme_cubit.dart';
 import 'package:democracy/notification/view/preferences.dart';
 import 'package:democracy/user/view/widgets/muted_and_blocked.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:material_symbols_icons/symbols.dart';
 
 class Settings extends StatelessWidget {
   const Settings({super.key});
@@ -11,6 +14,7 @@ class Settings extends StatelessWidget {
       appBar: AppBar(title: Text('Settings')),
       body: ListView(
         children: [
+          ThemeModeListTile(),
           ListTile(
             leading: const Icon(Icons.volume_mute),
             title: const Text('Mute and block'),
@@ -37,6 +41,91 @@ class Settings extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class ThemeModeListTile extends StatelessWidget {
+  const ThemeModeListTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ThemeCubit, ThemeState>(
+      builder: (context, state) {
+        List<bool> isSelected = [false, false, false];
+        ThemeMode themeMode = ThemeMode.system;
+        if (state.index != null) {
+          themeMode = ThemeMode.values[state.index!];
+        }
+        switch (themeMode) {
+          case ThemeMode.light:
+            isSelected = [true, false, false];
+            break;
+          case ThemeMode.system:
+            isSelected = [false, true, false];
+            break;
+          case ThemeMode.dark:
+            isSelected = [false, false, true];
+            break;
+        }
+        return ListTile(
+          leading: const Icon(Symbols.settings_brightness_rounded),
+          title: const Text('Theme'),
+          onTap: () {
+            switch (themeMode) {
+              case ThemeMode.light:
+                context.read<ThemeCubit>().change(
+                  index: ThemeMode.system.index,
+                );
+                break;
+              case ThemeMode.system:
+                context.read<ThemeCubit>().change(index: ThemeMode.dark.index);
+                break;
+              case ThemeMode.dark:
+                context.read<ThemeCubit>().change(index: ThemeMode.light.index);
+                break;
+            }
+          },
+          trailing: ThemeModeToggleButtons(isSelected: isSelected),
+        );
+      },
+    );
+  }
+}
+
+@immutable
+class ThemeModeToggleButtons extends StatelessWidget {
+  const ThemeModeToggleButtons({super.key, required this.isSelected});
+
+  final List<bool> isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return ToggleButtons(
+      isSelected: isSelected,
+      onPressed: (int newIndex) {
+        if (newIndex == 0) {
+          context.read<ThemeCubit>().change(index: ThemeMode.light.index);
+        } else if (newIndex == 1) {
+          context.read<ThemeCubit>().change(index: ThemeMode.system.index);
+        } else {
+          context.read<ThemeCubit>().change(index: ThemeMode.dark.index);
+        }
+      },
+      children: const <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 9.5),
+          child: Icon(Icons.wb_sunny),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 9.5),
+          child: Icon(Icons.phone_iphone),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 9.5),
+          child: Icon(Icons.bedtime),
+        ),
+      ],
     );
   }
 }
